c4bbc3466ef6bef5dc3e59badb466fd1d695b29f
Vinnie <34502840+vinicius426@users.noreply.github.com>
2022-04-13 19:42:27 +1000

    final changes on chat

--- src/screens/ChatScreen/index.js
+++ src/screens/ChatScreen/index.js
@@ -1,14 +1,24 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useContext } from "react";
 import { ChannelList } from "stream-chat-expo";
+import AuthContext from "../../contexts/Authentication";
 
 const ChatScreen = ({navigation}) => {
 
   const onChannelPressed = (channel) => {
     navigation.navigate('ChannelScreen', {channel})
   }
+
+  const {userId} = useContext(AuthContext);
+
+  const filters = {
+    members: {
+      $in: [userId],
+    },
+  };
+
   return (
-    <ChannelList onSelect={onChannelPressed}/>
+    <ChannelList onSelect={onChannelPressed} filters={filters}/>
   );
 };
 
